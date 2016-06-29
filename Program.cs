e68ab32c4c9688793402829c6ee386ef891e0e08
Sasha Goldshtein <goldshtn@gmail.com>
2016-06-29 16:55:02 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETLFlameGraph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
